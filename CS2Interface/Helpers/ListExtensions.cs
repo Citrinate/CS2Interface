266f04b6ece1eec8f3147cf14e49bf5d4a9c5012
Citrinate <phil4387@gmail.com>
2026-04-26 00:31:01 -0400

    Comments

--- CS2Interface/Helpers/ListExtensions.cs
+++ CS2Interface/Helpers/ListExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace CS2Interface {
 	public static class ListExtensions {
+		// Necessary for non-generic ASF support, which usually strips out Random.Shared.Shuffle
 		public static void Shuffle<T>(this IList<T> list) {
 			for (int i = list.Count - 1; i > 0; i--) {
 				int j = Random.Shared.Next(i + 1);
